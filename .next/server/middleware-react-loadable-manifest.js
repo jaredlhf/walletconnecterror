5a1389cbfa626e656fda6c6984059c860af1df27
jaredlhf <jlhfqaz@gmail.com>
2023-07-17 14:51:53 +0800

    add env example

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST={"../../node_modules/@txnlab/use-wallet/dist/esm/index.js -> algosdk":{"id":"../../node_modules/@txnlab/use-wallet/dist/esm/index.js -> algosdk","files":[]}}
